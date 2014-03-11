016667003649cedfa66ec599d9666433356bc31d
App Academy Student <student@appacademy.io>
2014-03-11 12:12:15 -0400

    basic board class and piece method plan

---
+++ board.rb
@@ -0,0 +1,22 @@
+class Board
+  attr_accessor :grid
+
+  def initialize(grid=Array.new(8) {Array.new(8)})
+    @grid = grid
+  end
+
+  #create factory method to generate pieces
+
+  def [](pos) #return error if position length != 2
+    x, y = pos
+    @grid[x][y]
+  end
+
+  def is_empty?(pos)
+    self[pos] == nil
+  end
+
+  def in_grid?(pos)
+    pos.all? {|coord| coord.between?(0..7)}
+  end
+end
--- piece.rb
+++ piece.rb
@@ -1,3 +1,5 @@
+require "board.rb"
+
 class Piece
   attr_accessor :position
   attr_reader :board, :color
@@ -7,9 +9,38 @@ class Piece
   end
 
   def moves
+    # will require piece's position & board
+
+    # call move_dirs || delta depending if it's sliding
+    # or stepping => array of all possible moves
+      # possible moves -- all moves in board
+
+    # select legal_moves and return as array
+    # legal moves -- all unblocked moves?
+      # sliding pieces paths are blocked by any piece in their path
+      # stepping pieces are blocked by what's @ the end of their path
   end
 
   def move_dirs
+
+    # add orthgonal
+      # based on position, add all orthogonal co-ordinates to arr_to_return
+
+    # add diagonal
+      # based on position, add all diagonal co-ordinates to arr_to_return
+
+  end
+
+  def delta_dirs
+
+    # deltas = [... some moves ...]
+
+    # each delta, add to pos and push result to arr_to_return
+
+  end
+
+  def select_legal_moves(poss_moves)
+    # select all legal moves from poss_moves and return array of positions
   end
 end
 
