0085fed4cb207161792df76774163938e0e80cbb
App Academy Student <student@appacademy.io>
2014-03-12 16:56:37 -0400

    broken piece, board refactor debug

--- board.rb
+++ board.rb
@@ -1,3 +1,7 @@
+# factory method -- board.setup
+# compact the one if-else
+# split up setup board [array of objects]
+
 class Board
   BOARD_WIDTH = 48
 
@@ -17,7 +21,7 @@ class Board
     self[[0, 6]] = Knight.new([0, 6], :B, self)
     self[[0, 7]] = Rook.new([0, 7], :B, self)
 
-    grid[1].count.times { |index| self[[1, index]] = Pawn.new([1,index], :B, self)}# make pawns
+    grid[1].count.times { |index| self[[1, index]] = Pawn.new([1,index], :B, self)} # make pawns
 
     self[[7, 0]] = Rook.new([7, 0], :W, self)
     self[[7, 1]] = Knight.new([7, 1], :W, self)
@@ -28,41 +32,28 @@ class Board
     self[[7, 6]] = Knight.new([7, 6], :W, self)
     self[[7, 7]] = Rook.new([7, 7], :W, self)
 
-
-    grid[6].count.times { |index| self[[6, index]] = Pawn.new([6,index], :W, self)}# make pawns
+    grid[6].count.times { |index| self[[6, index]] = Pawn.new([6,index], :W, self)} # make pawns
   end
 
-  def move(start_coord, end_coord)
-    piece_to_move = self[start_coord]
+  def move(start_coord, end_coord) # move should call move1
+    king_in_danger = IllegalMoveError.new("Your King is in Danger!")
+    raise king_in_danger if self[start_coord].move_into_check?(end_coord)
 
-    raise IllegalMoveError.new("Your King is in Danger!") if piece_to_move.move_into_check?(end_coord) #piece_to_move.valid_move?(end_coord)
-
-    self[start_coord], self[end_coord] = nil, piece_to_move
-    piece_to_move.position = end_coord
+    move!(start_coord, end_coord)
   end
 
   def move!(start_coord, end_coord)
-    piece_to_move = self[start_coord]
+    selected_piece = self[start_coord]
 
-    self[start_coord], self[end_coord] = nil, piece_to_move
-    piece_to_move.position = end_coord
+    self[start_coord], self[end_coord] = nil, selected_piece
+    selected_piece.position = end_coord
   end
 
   def checkmate?(color)
     if in_check?(color)
-      grid.each do |row|
-        row.each do |piece|
-          next if piece.nil?
-          if piece.color == color
-            piece.moves.each do |move|
-
-              return false unless piece.move_into_check?(move)
-
-            end
-          end
-        end
+      all_pieces_of_color(color) do |pc|
+        pc.moves.each{ |move| return false unless pc.move_into_check?(move)}
       end
-
       return true
     end
 
@@ -74,7 +65,7 @@ class Board
       print "  "; BOARD_WIDTH.times{print "-"}; puts ""
       print "#{(num-7).abs} "
       row.each do |tile|
-        if tile.nil?
+        if tile.nil? # compact if-else
           print "|    |"
         else
           print "| #{tile.inspect} |"
@@ -89,42 +80,34 @@ class Board
   end
 
   def in_check?(color)
-    opposite_color = []
-    king_position = []
-
-    grid.each do |row|
-      row.each do |piece|
-        next if piece.nil?
-        king_position = piece.position if piece.instance_of?(King) && piece.color == color
-        opposite_color << piece if piece.color != color
-      end
+    king_pos = []
+    all_pieces_of_color(color) do |pc|
+      king_pos = pc.position if pc.instance_of?(King)
     end
 
-    opposite_color.each do |enemy|
-      return true if enemy.moves.include?(king_position)
-    end
+    opp_color = []
+    all_pieces_not_of_color(color){|pc| opp_color << pc}
+    opp_color.each{|enemy| return true if enemy.moves.include?(king_pos)}
 
     false
   end
 
   def deep_dup
     new_board = Board.new
-    grid.each do |row|
-      row.each do |piece|
-        next if piece.nil?
-        piece_new = piece.class.new(piece.position.dup, piece.color, new_board)
-        new_board[piece.position] = piece_new
-      end
+
+    all_pieces do |pc|
+      piece_new = pc.class.new(pc.position.dup, pc.color, new_board)
+      new_board[pc.position] = piece_new
     end
     new_board
   end
 
-  def [](pos) #return error if position length != 2
-    x, y = pos # REMEMBER TO SWAP CO-ORDINATES IN PARSE COMMAND
+  def [](pos)
+    x, y = pos
     @grid[x][y]
   end
 
-  def []=(pos, piece) #return error if position length != 2
+  def []=(pos, piece)
     x, y = pos
     @grid[x][y] = piece
   end
@@ -137,8 +120,22 @@ class Board
     pos.all? {|coord| coord.between?(0, 7)}
   end
 
-  def color_at(pos)
-    self[pos].color
+  private
+  def all_pieces_of_color(color, &block)
+    all_pieces { |piece| block.call(piece) if piece.color == color}
+  end
+
+  def all_pieces_not_of_color(color, &block)
+    all_pieces { |piece| block.call(piece) unless piece.color == color}
+  end
+
+  def all_pieces(&block)
+    grid.each do |row|
+      row.each do |piece|
+        next if piece.nil?
+        block.call(piece)
+      end
+    end
   end
 end
 
--- piece.rb
+++ piece.rb
@@ -7,9 +7,6 @@ class Piece
   L_SHAPED = [[1, 2], [1, -2], [-1, -2], [-1, 2],
               [2, 1], [-2, 1], [-2, -1], [2, 1]]
 
-  KING = [[1, 0], [-1, 0], [0, 1], [0, -1],
-          [1, 1], [-1, -1],[1, -1],[-1, 1]]
-
   attr_accessor :position
   attr_reader :board, :color
 
@@ -18,7 +15,7 @@ class Piece
     board[position] = self
   end
 
-  def is_enemy?(pos)
+  def is_enemy?(pos)                    # for board?
     return false if board[pos].nil?
     board[pos].color != self.color
   end
@@ -28,81 +25,58 @@ class Piece
     new_board.move!(self.position, pos)
     new_board.in_check?(self.color)
   end
-
-  # helper methods
 end
 
 class SlidingPiece < Piece
-
-  def add_diagonal_moves
-    # debugger
-    diagonal_moves = []
-    DIAGONALS.each do |diag|
+  def get_sliding_moves(directions)
+    moves = []
+    directions.each do |dir|
       i = 1
-      new_pos = [(position.first + (diag.first * i)), (position.last + (diag.last * i))]
+      new_pos = [position.first + dir.first, position.last + dir.last]
       while board.in_grid?(new_pos)
-        if board.is_empty?(new_pos)
-          diagonal_moves << new_pos
-        elsif is_enemy?(new_pos)
-          diagonal_moves << new_pos
-          break
-        else
-          break
-        end
+        moves << new_pos if board.is_empty?(new_pos) || is_enemy?(new_pos)
+        break unless board.is_empty?(new_pos)
         i += 1
-        new_pos = [position.first + (diag.first * i), position.last + (diag.last * i)]
+        new_pos = [position.first + (dir.first * i), position.last + (dir.last * i)]
       end
     end
-    diagonal_moves
-  end
 
-  def add_orthogonal_moves
-    orthogonal_moves = []
-    ORTHOGONALS.each do |orth|
-      i = 1
-      new_pos = [position.first + (orth.first * i), position.last + (orth.last * i)]
-      while board.in_grid?(new_pos)
-        if board.is_empty?(new_pos)
-          orthogonal_moves << new_pos
-        elsif is_enemy?(new_pos)
-          orthogonal_moves << new_pos
-          break
-        else
-          break
-        end
-        i += 1
-        new_pos = [position.first + (orth.first * i), position.last + (orth.last * i)]
-      end
-    end
-    orthogonal_moves
+    moves
   end
 end
 
 class Pawn < Piece
+  PAWN_MOVES_W = [[-1, 0], [-2, 0], [-1, -1], [-1, 1]]
+  PAWN_MOVES_B = [[1, 0], [2, 0], [1, 1], [1, -1]]
+
   def moves
-    the_moves = []
+    moves = []
     case color
     when :W
-      one_forward = [position.first - 1, position.last]
-      two_forward = [position.first - 2, position.last]
-      capture_left = [position.first - 1, position.last - 1]
-      capture_right = [position.first - 1, position.last + 1]
+      moves = get_pawn_moves(PAWN_MOVES_W)
 
-      the_moves << one_forward if board.is_empty?(one_forward)
-      the_moves << two_forward if position.first == 6 && (board.is_empty?(one_forward) && board.is_empty?(two_forward))
-      the_moves << capture_left if board.in_grid?(capture_left) && is_enemy?(capture_left)
-      the_moves << capture_right if board.in_grid?(capture_right) && is_enemy?(capture_right)
+      # one_forward = [position.first - 1, position.last]
+ #      two_forward = [position.first - 2, position.last]
+ #      capture_left = [position.first - 1, position.last - 1]
+ #      capture_right = [position.first - 1, position.last + 1]
+ #
+ #      the_moves << one_forward if board.is_empty?(one_forward)
+ #      the_moves << two_forward if position.first == 6 && (board.is_empty?(one_forward) && board.is_empty?(two_forward))
+ #      the_moves << capture_left if board.in_grid?(capture_left) && is_enemy?(capture_left)
+ #      the_moves << capture_right if board.in_grid?(capture_right) && is_enemy?(capture_right)
 
     when :B
-      one_forward = [position.first + 1, position.last]
-      two_forward = [position.first + 2, position.last]
-      capture_left = [position.first + 1, position.last + 1]
-      capture_right = [position.first + 1, position.last - 1]
+      moves = get_pawn_moves(PAWN_MOVES_B)
 
-      the_moves << one_forward if board.is_empty?(one_forward)
-      the_moves << two_forward if position.first == 1 && (board.is_empty?(one_forward) && board.is_empty?(two_forward))
-      the_moves << capture_left if board.in_grid?(capture_left) && is_enemy?(capture_left)
-      the_moves << capture_right if board.in_grid?(capture_right) && is_enemy?(capture_right)
+      # one_forward = [position.first + 1, position.last]
+      # two_forward = [position.first + 2, position.last]
+      # capture_left = [position.first + 1, position.last + 1]
+      # capture_right = [position.first + 1, position.last - 1]
+      #
+      # the_moves << one_forward if board.is_empty?(one_forward)
+      # the_moves << two_forward if position.first == 1 && (board.is_empty?(one_forward) && board.is_empty?(two_forward))
+      # the_moves << capture_left if board.in_grid?(capture_left) && is_enemy?(capture_left)
+      # the_moves << capture_right if board.in_grid?(capture_right) && is_enemy?(capture_right)
     end
     the_moves
   end
@@ -126,7 +100,7 @@ class SteppingPiece < Piece
 
   def add_king_moves
     king_moves = []
-    KING.each do |k_mov|
+    (DIAGONALS + ORTHOGONALS).each do |k_mov|
       new_pos = [position.first + k_mov.first, position.last + k_mov.last]
       king_moves << new_pos if board.in_grid?(new_pos) && (board.is_empty?(new_pos) || is_enemy?(new_pos))
     end
@@ -137,7 +111,7 @@ end
 class Queen < SlidingPiece
 
   def moves
-    add_diagonal_moves + add_orthogonal_moves
+    get_sliding_moves(DIAGONALS + ORTHOGONALS)
   end
 
   def inspect
@@ -149,7 +123,7 @@ end
 class Bishop < SlidingPiece
 
   def moves
-    add_diagonal_moves
+    get_sliding_moves(DIAGONALS)
   end
 
   def inspect
@@ -161,7 +135,7 @@ end
 class Rook < SlidingPiece
 
   def moves
-    add_orthogonal_moves
+    get_sliding_moves(ORTHOGONALS)
   end
 
   def inspect
