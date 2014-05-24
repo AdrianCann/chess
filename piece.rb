108d66d05089d49690a5fc5327bc5a0bbf4b4215
Adrian Cann <adriantcann@gmail.com>
2014-05-24 19:43:56 -0400

    Adds changes from second day

--- piece.rb
+++ piece.rb
@@ -1,113 +1,164 @@
-require "board.rb"
+require "./colorize"
 
 class Piece
 
-  DIAGONALS = [].tap do |diags|
-    (-8..8).to_a.each do |coord|
-      diags << [coord, coord]
-      diags << [-coord, coord]
-    end.delete([0, 0])
-  end
-
-  ORTHOGONALS = [].tap do |orths|
-    (-8..8).to_a.each do |coord|
-      orths << [coord, 0]
-      orths << [0, coord]
-    end.delete([0, 0])
-  end
-
-  # kill them with cleverness
-  L_SHAPED = [1, -1, 2, -2].product([-2, 2, -1, 1]).select{|(x, y)| x.abs + y.abs == 3}
-
-  KING = [1, 0, -1].product([1, 0, -1]).delete([0, 0])
+  DIAGONALS = [[1, 1], [-1, 1], [-1, -1], [1, -1]]
+  ORTHOGONALS = [[0, 1], [0, -1], [1, 0], [-1, 0]]
 
   attr_accessor :position
   attr_reader :board, :color
 
   def initialize(position, color, board)
     @position, @color, @board = position, color, board
+    board[position] = self
   end
 
-  # helper methods
+  def is_enemy?(pos)                    # for board?
+    return false if board[pos].nil?
+    board[pos].color != self.color
+  end
+
+  def move_into_check?(pos)
+    new_board = self.board.deep_dup
+    new_board.move!(self.position, pos)
+    new_board.in_check?(self.color)
+  end
+
+  private
+  def delta(dir, mod = 1)
+    [position.first + (dir.first * mod), position.last + (dir.last * mod)]
+  end
+end
+
+class Pawn < Piece
+  PAWN_MOVES_W = [[-1, 0], [-2, 0], [-1, -1], [-1, 1]]
+  PAWN_MOVES_B = [[1, 0], [2, 0], [1, 1], [1, -1]]
 
   def moves
-    #get deltas sums up all deltas for a sublcass
-    pos_moves = deltas.map do |delta|
-      [position.first + delta.first, position.last + delta.last]
-    end.select do |coords|
-      board.in_grid?(coords)
+    moves = []
+    case color
+    when :white
+      moves = get_pawn_moves(PAWN_MOVES_W)
+    when :light_black
+      moves = get_pawn_moves(PAWN_MOVES_B)
     end
+    moves
+  end
 
-    sel_legal_moves(pos_moves) # overide in slide/step subclass
+  def inspect
+    "P".colorize(color)
+  end
 
-    # select legal_moves and return as array
-    # legal moves -- all unblocked moves?
-      # sliding pieces paths are blocked by any piece in their path
-      # stepping pieces are blocked by what's @ the end of their path
+  private
+  def get_pawn_moves(mvs)  #delta takes an argument (always positive?)
+    moves = []
+    copy = mvs.dup
+    copy.map!{ |dir| delta(dir) }.select!{|mv| board.in_grid?(mv) }
+
+    moves << copy[0] if board.is_empty?(copy[0])
+    if color == :light_black && position.first == 1 || color == :white && position.first == 6
+      moves << copy[1] if board.is_empty?(copy[0]) && board.is_empty?(copy[1])
+    end
+    moves << copy[2] if is_enemy?(copy[2])
+    moves << copy[3] if is_enemy?(copy[2])
+
+    moves
   end
 end
 
 class SlidingPiece < Piece
+  private
+  def get_sliding_moves(directions)
+    moves = []
+    directions.each do |dir|
+      i = 1
+      new_pos = delta(dir)
+      while board.in_grid?(new_pos)
+        moves << new_pos if board.is_empty?(new_pos) || is_enemy?(new_pos)
+        break unless board.is_empty?(new_pos)
+        i += 1
+        new_pos = delta(dir, i)
+      end
+    end
 
-  def select_legal_moves(poss_moves)
-    # select all legal moves from poss_moves (based on @board) and return array of positions
+    moves
   end
-
 end
 
 class SteppingPiece < Piece
+  private
+  def get_stepping_moves(directions)
+    moves = []
+    directions.each do |dir|
+      new_pos = delta(dir)
+      if board.in_grid?(new_pos)
+        moves << new_pos if board.is_empty?(new_pos) || is_enemy?(new_pos)
+      end
+    end
 
+    moves
+  end
+end
 
+class Queen < SlidingPiece
 
+  def moves
+    get_sliding_moves(DIAGONALS + ORTHOGONALS)
+  end
 
-  def select_legal_moves(poss_moves)
-    # select all legal moves from poss_moves (based on @board) and return array of positions
+  def inspect
+    "Q".colorize(color)
   end
 
 end
 
-class Queen < SlidingPiece
-  DIAGONALS && ORTHOGONALS
-end
-
 class Bishop < SlidingPiece
-  DIAGONALS
+
+  def moves
+    get_sliding_moves(DIAGONALS)
+  end
+
+  def inspect
+    "B".colorize(color)
+  end
+
 end
 
 class Rook < SlidingPiece
-  ORTHOGONALS
+
+  def moves
+    get_sliding_moves(ORTHOGONALS)
+  end
+
+  def inspect
+    "R".colorize(color)
+  end
+
 end
 
 class Knight < SteppingPiece
-  L-SHAPED
+
+  L_SHAPED = [[1, 2], [1, -2], [-1, -2], [-1, 2],
+              [2, 1], [-2, 1], [-2, -1], [2, 1]]
+
+  def moves
+    get_stepping_moves(L_SHAPED)
+  end
+
+  def inspect
+    "N".colorize(color)
+  end
+
 end
 
 class King < SteppingPiece
-  KING
+
+  def moves
+    get_stepping_moves(ORTHOGONALS + DIAGONALS)
+  end
+
+  def inspect
+    "K".colorize(color)
+  end
+
 end
-
-
-
-
-=begin
-
-DIAGONALS
-
-ORTHOGONALS
-
-L-SHAPED
-
-KING
-
-
-diagonals
-
-[1,1], [2,2], [3,3] ... [8,8]
-[1, -1] ... [8, -8]
-...
-
-possible moves << coord + delta
-
-
-
-=end
